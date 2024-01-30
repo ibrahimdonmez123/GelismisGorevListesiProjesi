beaecdec0988a9621c892aec8b3dd7e21e01896c
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 23:33:14 +0300

    Add project files.

---
+++ GelismisGorevListesiProjesi/MainMenu.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GelismisGorevListesiProjesi
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btn_save_click = new System.Windows.Forms.Button();
+            this.btn_list = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(141, 143);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(195, 20);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // btn_save_click
+            // 
+            this.btn_save_click.Location = new System.Drawing.Point(131, 268);
+            this.btn_save_click.Name = "btn_save_click";
+            this.btn_save_click.Size = new System.Drawing.Size(205, 68);
+            this.btn_save_click.TabIndex = 2;
+            this.btn_save_click.Text = "NOTLARA EKLE";
+            this.btn_save_click.UseVisualStyleBackColor = true;
+            this.btn_save_click.Click += new System.EventHandler(this.btn_save_click_Click);
+            // 
+            // btn_list
+            // 
+            this.btn_list.Location = new System.Drawing.Point(364, 268);
+            this.btn_list.Name = "btn_list";
+            this.btn_list.Size = new System.Drawing.Size(205, 68);
+            this.btn_list.TabIndex = 2;
+            this.btn_list.Text = "NOTLARI GÖSTER";
+            this.btn_list.UseVisualStyleBackColor = true;
+            this.btn_list.Click += new System.EventHandler(this.btn_list_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(364, 143);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(368, 20);
+            this.txtContent.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(145, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Başlığı Gir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(369, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 39);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "İçeriği Gir";
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_list);
+            this.Controls.Add(this.btn_save_click);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainMenu_FormClosing);
+            this.Load += new System.EventHandler(this.MainMenu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btn_save_click;
+        private System.Windows.Forms.Button btn_list;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ GelismisGorevListesiProjesi/MainMenu.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GelismisGorevListesiProjesi
+{
+    public partial class MainMenu : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-JI1JKUA\\SQLEXPRESS;Initial Catalog=NotDefter;Integrated Security=True");
+
+
+
+        public MainMenu()
+        {
+            InitializeComponent();
+
+            
+        }
+
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_save_click_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            string content = txtContent.Text;
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand($"INSERT INTO Notes (Title, Content) VALUES ('{title}', '{content}')", conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Not başarıyla kaydedildi.");
+            txtTitle.Text = "";
+            txtContent.Text = "";
+            conn.Close();
+        }
+
+        private void btn_list_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            Form listForm = new Form();
+            listForm.Size = new Size(300, 500);
+            listForm.StartPosition = FormStartPosition.CenterScreen;
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Notes", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            ListBox listBox = new ListBox();
+            while (reader.Read())
+            {
+                listBox.Items.Add($"{reader["Title"]}: {reader["Content"]}");
+            }
+
+            listForm.Controls.Add(listBox);
+            listForm.ShowDialog();
+            reader.Close();
+            conn.Close();
+
+        }
+
+        private void MainMenu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
